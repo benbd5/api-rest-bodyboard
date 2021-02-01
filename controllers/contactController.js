99e515ab7355b5d2b7e592c4baab0947bac63397
benbd5 <belzbenjaminp@gmail.com>
2021-02-01 15:40:14 +0100

    formulaire de contact opérationnel

---
+++ controllers/contactController.js
@@ -0,0 +1,52 @@
+const nodemailer = require("nodemailer");
+
+const contact_get = (req, res) => {
+  res.locals.title = "Contact";
+
+  res.render("forms/contact");
+};
+
+const sendMessage = async (req, res) => {
+  const output = `
+    <p>Nouvelle requête :</p>
+      <h3>Contact</h3>
+      <ul>  
+        <li>Nom: ${req.body.lastName}</li>
+        <li>Prénom: ${req.body.firstName}</li>
+        <li>Email: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <h4>Sujet : ${req.body.subject}</h4>
+      <p>${req.body.message}</p>
+    `;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    service: "hotmail",
+    auth: {
+      user: process.env.HOST,
+      pass: process.env.HOST_MDP,
+    },
+    tls: { rejectUnauthorized: false },
+  });
+
+  const msg = {
+    from: process.env.HOST,
+    to: process.env.TO_MAIL,
+    subject: "Requête contact",
+    text: "Vous avez reçu une requête",
+    html: output,
+  };
+
+  transporter.sendMail(msg, function (err, info) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Sent" + info.response);
+    res.send("Message envoyé !");
+  });
+};
+
+module.exports = { sendMessage, contact_get };
--- routes/contact.js
+++ routes/contact.js
@@ -1,42 +1,12 @@
 const express = require("express");
 const router = express.Router();
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
+
+const contactController = require("../controllers/contactController");
 
 // GET home page
-router.get("/forms/contact", (req, res) => {
-  res.locals.title = "Contact";
+router.get("/forms/contact", contactController.contact_get);
 
-  res.render("forms/contact");
-});
+router.post("/forms/send", contactController.sendMessage);
 
-/*
-router.post("/forms/send", (req, res) => {
-  var transporter = nodemailer.createTransport({
-    service: "smtp.gmail.com",
-    port: 587,
-    secure: true,
-    pool: true,
-    auth: {
-      user: process.env.USER,
-      pass: process.env.MDP,
-    },
-  });
-
-  var mailOptions = {
-    from: "benjibdu85@gmail.com",
-    to: "barbara.marchand.5@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
-  //   transporter.close();
-});
-*/
 module.exports = router;
